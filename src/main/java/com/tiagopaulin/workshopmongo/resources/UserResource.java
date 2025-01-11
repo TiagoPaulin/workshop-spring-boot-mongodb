dcd920b584ae22361973825bbf740359c8bc8199
TiagoPaulin <thiago.vmp15@gmail.com>
2025-01-10 21:18:44 -0300

    UserDTO

--- src/main/java/com/tiagopaulin/workshopmongo/resources/UserResource.java
+++ src/main/java/com/tiagopaulin/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.tiagopaulin.workshopmongo.resources;
 
 import com.tiagopaulin.workshopmongo.domain.User;
+import com.tiagopaulin.workshopmongo.dto.UserDTO;
 import com.tiagopaulin.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -20,9 +22,11 @@ public class UserResource {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
 
-        List<User> users = service.findAll();
+        List<User> list = service.findAll();
+        List<UserDTO> users = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+
         return ResponseEntity.ok().body(users);
 
     }
