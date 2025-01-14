742fd96a9c8c7e605e283b7cbf573b15fca625f1
TiagoPaulin <thiago.vmp15@gmail.com>
2025-01-13 21:43:53 -0300

    Entity Post

--- src/main/java/com/tiagopaulin/workshopmongo/config/Instantiation.java
+++ src/main/java/com/tiagopaulin/workshopmongo/config/Instantiation.java
@@ -1,30 +1,46 @@
 package com.tiagopaulin.workshopmongo.config;
 
+import com.tiagopaulin.workshopmongo.domain.Post;
 import com.tiagopaulin.workshopmongo.domain.User;
+import com.tiagopaulin.workshopmongo.repositories.PostRepository;
 import com.tiagopaulin.workshopmongo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
 
     @Autowired
-    private UserRepository repository;
+    private UserRepository userRepository;
+
+    @Autowired
+    private PostRepository postRepository;
 
     @Override
     public void run(String... args) throws Exception {
 
-        repository.deleteAll();
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
+        userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User tiago = new User(null, "Tiago Paulin", "tiago@gmail.com");
         User gabriel = new User(null, "Gabriel Felipe", "gabriel@gmail.com");
         User tatsuo = new User(null, "Brunno Tatsuo", "brunno@gmail.com");
         User sid = new User(null, "Sidnei Henrique", "sid@gmail.com");
 
-        repository.saveAll(Arrays.asList(tiago, gabriel, tatsuo, sid));
+        userRepository.saveAll(Arrays.asList(tiago, gabriel, tatsuo, sid));
+
+        Post post1 = new Post(null, sdf.parse("13/01/2025"), "Viagem com os amigos", "Vamos viajar com os amigos semana que vem", tiago);
+        Post post2 = new Post(null, sdf.parse("12/01/2025"), "Meu aniversário está chegando", "Faltam apenas 5 dias para meu aniversário", tiago);
+
+        postRepository.saveAll(Arrays.asList(post1,post2));
 
     }
 }
